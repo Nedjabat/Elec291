53e68b35bf5a84f069da4869b324ff75c3615542
Nedjabat <56488093+Nedjabat@users.noreply.github.com>
2022-02-08 14:43:36 -0800

    Update Project 1 capacitor.asm

--- Project 1 capacitor.asm
+++ Project 1 capacitor.asm
@@ -1,3 +1,78 @@
 $NOLIST
 $MODLP51
 $LIST
+
+org 0000H
+   ljmp MyProgram
+
+DSEG at 30H
+x:   ds 4
+y:   ds 4
+seed: ds 4  
+bcd: ds 5
+
+BSEG
+mf: dbit 1
+cseg
+; These 'equ' must match the hardware wiring
+LCD_RS equ P3.2
+;LCD_RW equ PX.X ; Not used in this code, connect the pin to GND
+LCD_E  equ P3.3
+LCD_D4 equ P3.4
+LCD_D5 equ P3.5
+LCD_D6 equ P3.6
+LCD_D7 equ P3.7
+
+Initial_Message:  db 'Player1:', 0
+Initial_Message2: db 'Player2:', 0
+
+Wait1s:
+    mov R2, #176
+X3: mov R1, #250
+X2: mov R0, #166
+X1: djnz R0, X1 ; 3 cycles->3*45.21123ns*166=22.51519us
+    djnz R1, X2 ; 22.51519us*250=5.629ms
+    djnz R2, X3 ; 5.629ms*176=1.0s (approximately)
+    ret
+
+random:
+    mov x+0, seed+0
+    mov x+1, seed+1
+    mov x+2, seed+2
+    mov x+3, seed+3
+    Load_y()
+    lcall mul32
+    Load_y()
+    lcall add32
+    mov seed+0, x+0
+    mov seed+1, x+1
+    mov seed+2, x+2
+    mov seed+3, x+3
+    ret
+wait_random:
+    Wait_Milli_Seconds(seed+0)
+    Wait_Milli_Seconds(seed+1)
+    Wait_Milli_Seconds(seed+2)
+    Wait_Milli_Seconds(seed+3)
+    ret
+
+MyProgram:
+    Set_Cursor(1, 1)
+    Send_Constant_String(#Initial_Message)
+    Set_Cursor(2, 1)
+    Send_Constant_String(#Initial_Message2)
+    setb TR2
+    jb P4.5, $
+    mov seed+0, TH2
+    mov seed+1, #0x01
+    mov seed+2, #0x87
+    mov seed+3, TL2
+    clr TR2
+
+loop:
+    jb START_BUTTON, start_game
+    Wait_Milli_Seconds(#50)
+    jb START_BUTTON, start_game
+    jnb START_BUTTON, $
+
+start_game:
